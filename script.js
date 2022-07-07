2f3c55d6387764c28092d64ffb41ff8f2e8f1175
greg@gregPredator <greg19850@gmail.com>
2022-07-07 19:18:32 +0100

    created initiate function

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ let count = 1;
 
 
 const initiate = function () {
+    const btn = document.createElement('button');
+    btn.textContent = 'Press me'
+    const list = document.createElement('ul');
+
+    document.body.appendChild(btn);
+    document.body.appendChild(list);
 
 }
-
-
 
 const addLiElement = function () {
 

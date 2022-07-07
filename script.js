757b7ff680493ca56cc27ebc1452c69835a88992
greg@gregPredator <greg19850@gmail.com>
2022-07-07 19:33:48 +0100

    updated initiate function, created addLiEleent function

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ let count = 1;
 const initiate = function () {
     const btn = document.createElement('button');
     btn.textContent = 'Press me'
-    const list = document.createElement('ul');
-
     document.body.appendChild(btn);
-    document.body.appendChild(list);
 
+    btn.addEventListener('click', addLiElement)
 }
 
 const addLiElement = function () {
+    const list = document.createElement('ul');
+    document.body.appendChild(list);
+    for (let i = 0; i < 10; i++) {
+        const liElement = document.createElement('li');
+        liElement.textContent = count;
+        liElement.style.fontSize = `${size}px`
+        list.appendChild(liElement)
+        count++;
+        size += 2
+    }
 
 }
 
 
-initiate()
+initiate(addLiElement)
